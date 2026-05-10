7a6f5ed55fe0d60a7d98b618a8e9aeba192b842e
Vivid-Vortex <ctntc9.dpk@gmail.com>
2026-05-10 18:09:40 +0530

    added conext related codes

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,50 @@
 import { College } from './components/College';
+import { SubjectContext } from './context/ContextData';
+import { useState } from 'react';
 
 function App() {
+  const [subject, setSubject] = useState('English');
   return (
     <div style={{ backgroundColor: 'yellow', padding: '10px' }}>
-      <h1>Context API</h1>
-      <College />
+      {/* Using value=English to directly sending data (English) from App to
+      Subject Component.  */}
+      <SubjectContext.Provider value={subject}>
+        {/* <select>
+          <option value="">Select Subject</option>
+          <option value="Maths">Maths</option>
+          <option value="History">History</option>
+          <option value="English">English</option>
+        </select> */}
+
+        {/* <select onChange={(event) => setSubject(event.target.value)}>
+          <option value="">Select Subject</option>
+          <option value="Maths">Maths</option>
+          <option value="History">History</option>
+          <option value="English">English</option>
+        </select> */}
+
+        {/* <select
+          defaultValue={subject}
+          onChange={(event) => setSubject(event.target.value)}
+        >
+          <option value="">Select Subject</option>
+          <option value="Maths">Maths</option>
+          <option value="History">History</option>
+          <option value="English">English</option>
+        </select> */}
+
+        <select
+          value={subject}
+          onChange={(event) => setSubject(event.target.value)}
+        >
+          <option value="">Select Subject</option>
+          <option value="Maths">Maths</option>
+          <option value="History">History</option>
+          <option value="English">English</option>
+        </select>
+        <button onClick={() => setSubject('')}>Clear Subject</button>
+        <College />
+      </SubjectContext.Provider>
     </div>
   );
 }
--- src/components/Subject.jsx
+++ src/components/Subject.jsx
@@ -1,7 +1,11 @@
+import { useContext } from 'react';
+import { SubjectContext } from '../context/ContextData';
+
 export function Subject() {
+  const subject = useContext(SubjectContext);
   return (
     <div style={{ backgroundColor: 'red', padding: '10px' }}>
-      <h1>Subject Component</h1>
+      <h1>Subject is: {subject}</h1>
     </div>
   );
 }
